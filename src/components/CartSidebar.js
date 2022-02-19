397935bcfe9d9bffa665f9cf7562009aba97ddea
Jeff Morton <jmorton138@gmail.com>
2022-02-19 08:15:10 +0000

    Updated styles and layout of sidebar. Checkout button. Added subtotal calculator. Adjusted spacing

--- src/components/CartSidebar.js
+++ src/components/CartSidebar.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
 const CartSidebar = (props) => {
   let cartRender;
+  let noItems;
+  let subtotal = 0;
+  let checkout;
   if (props.cart.length === 0) {
-    cartRender = <div className="no-items">No items in Cart</div>;
+    noItems = "No items in cart";
   } else {
     cartRender = props.cart.map((item) => {
+      let orderCost = item.price * item.quantity;
+      subtotal = subtotal + orderCost;
       return (
         <div className="checkout-item">
           <img className="checkout-item-image" src={item.imgUrl}></img>
@@ -16,30 +21,36 @@ const CartSidebar = (props) => {
           <div className="checkout-item-name">{item.name}</div>
           <div className="checkout-item-price">${item.price}</div>
 
-          <button
+          <div
             className="checkout-delete"
             onClick={(e) => {
               props.deleteFromCart(e, item.id);
             }}
           >
             <FontAwesomeIcon icon={faTrashCan} />
-          </button>
+          </div>
         </div>
       );
     });
+    checkout = (
+      <div className="checkout-btn-wrap">
+        <div className="subtotal">Subtotal: ${subtotal}</div>
+        <button className="checkout-btn">Checkout</button>
+      </div>
+    );
   }
 
   return (
     <div>
-      <button
+      <div
         className="close-sidebar-btn"
         onClick={(e) => props.hideCartToggle(e)}
       >
-        X
-      </button>
-
-      {cartRender}
-      <button>Checkout</button>
+        <FontAwesomeIcon icon={faCircleXmark} />
+      </div>
+      <div className="no-items">{noItems}</div>
+      <div>{cartRender}</div>
+      <div>{checkout}</div>
     </div>
   );
 };
